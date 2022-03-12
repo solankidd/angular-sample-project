18a2976bb3676134ce3b8888d0842544358acfdb
solankidd <dhaval.d.solanki@gmail.com>
2022-03-12 17:28:25 +0530

    1 file modified, 1 added

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
