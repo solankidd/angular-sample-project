8af781a8e8eb1cf9ab9d5343f4d766845fa5da6a
solankidd <dhaval.d.solanki@gmail.com>
2022-03-12 17:14:20 +0530

    1 file modified

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getGameList(ordering: string, search?: string): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+    if(search){
+      params = new HttpParams().set('ordering', ordering).('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params
+    });
+  }
 
 }
