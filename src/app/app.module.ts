0c3ecc57374e58d2c1fbc9ad7def3a16a3cfa64e
solankidd <dhaval.d.solanki@gmail.com>
2022-03-12 16:02:21 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -17,6 +18,7 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
     BrowserModule, 
     FormsModule,
     BrowserModule,
+    AppRoutingModule,
     GaugeModule.forRoot(),
     HttpClientModule,
     FormsModule,
