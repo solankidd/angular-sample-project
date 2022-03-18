3dbf04357c0c88e9b9c0c0c4c38d327c4f9587f3
solankidd <dhaval.d.solanki@gmail.com>
2022-03-18 12:16:39 +0530

    3 files modified

--- src/app/check-service/check-service/check-service.component.ts
+++ src/app/check-service/check-service/check-service.component.ts
@@ -5,15 +5,16 @@ import { ApiService } from '../../services/api.service';
   selector: 'app-check-service',
   templateUrl: './check-service.component.html',
   styleUrls: ['./check-service.component.css'],
-  // providers: [ApiService]
+  providers: [ApiService]
 })
-export class CheckServiceComponent implements OnInit { 
+export class CheckServiceComponent implements OnInit {  
 
   constructor(
     private api: ApiService
   ) { }
 
   ngOnInit() {
+    console.log('Test')
   }
 
 }
